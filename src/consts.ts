5a7903a23a0a2fdcfe8260cd255cdc7e917539b9
Nepal Rameshwor <nepalrameshwor011@gmail.com>
2024-05-28 21:28:32 +0545

    list of pokeman

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pokeman Site';
+export const SITE_DESCRIPTION = 'Welcome to  pokeman site!';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue: {
+					1: '#162154',
+					2: '#375c9d',
+					3: '#267fe0',
+					4: "#f2f8ff",
+				},
+				cyan: {
+					1: "#34d6ff",
+				},
+				gray: {
+					1: '#2c262e',
+					2: '#4d5054',
+					3: '#d7d7d9',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
